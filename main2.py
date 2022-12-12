627c6fecb6d0ce084c540f43a1ebef9a129dd14c
Anak Rumah <75354525+AnakRumah@users.noreply.github.com>
2022-12-13 00:42:34 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -65,13 +65,5 @@ def main():
                 file_name="DATA RUMAH.xlsx",
                 mime="xlsx"
             )
-
-
-
-if __name__ == '__main__':
-    if st.runtime.exists():
-        main()
-    else:
-        sys.argv = ["streamlit", "run", sys.argv[0]]
-        sys.exit(stcli.main())
-
+        
+main()
